8ebae1e32c67d0d5f72dd3cc1cf7ead05f4e5c9f
Jason <yuchenshi327@outlook.com>
2024-05-15 15:03:43 +0800

    push it

---
+++ Final pro/Room.h
@@ -0,0 +1,24 @@
+/*
+ * @Author: Yuchen Shi
+ * @Date: 29-04-2024 19:28:37
+ * @LastEditors: Jason
+ * @Contact LastEditors: yuchenshi327@outlook.com
+ * @LastEditTime: 15-05-2024 14:53:10
+ */
+
+#ifndef ROOM_H
+#define ROOM_H
+
+#include <iostream>
+
+// 声明结构体Room
+struct Room
+{
+	// 設定房間基本信息
+	int number;
+	int rank;
+	int state;
+	int price;
+};
+
+#endif // ROOM_H
